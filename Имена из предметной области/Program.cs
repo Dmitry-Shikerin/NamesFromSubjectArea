c725936718765d0f354a0cfab14f59e300889e2d
Dmitry-Shikerin <130661730+Dmitry-Shikerin@users.noreply.github.com>
2023-10-05 15:24:34 +0300

    Добавьте файлы проекта.

---
+++ Имена из предметной области/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Имена_из_предметной_области
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Weapon
+    {
+        public void Shoot() { }
+    }
+}
